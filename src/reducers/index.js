e87c129fa96f3192618dd293989a68b8e9af4291
Byron <byron@Sinleads-MacBook-Air.local>
2020-11-30 18:18:27 +0800

    in progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-
+import postsReducer from './postsReducer';
 export default combineReducers({
-  replaceMe: () =>  { return 'test'}
+  posts: postsReducer
 });
